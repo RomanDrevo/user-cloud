b26a36fd373de8956543157ed9916b65d502683d
Roman <romulus.hund@gmail.com>
2020-08-11 12:54:35 +0300

    user card WIP

--- src/components/user-card/UserCard.js
+++ src/components/user-card/UserCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import style from './UserCard.module.scss';
 import {Divider} from 'antd';
+import {MailOutlined, DeleteOutlined} from '@ant-design/icons';
+
 
 const UserCard = () => {
     return(
@@ -19,6 +21,16 @@ const UserCard = () => {
                     <div className='user-id'>ID: 1234757499</div>
                     <div className='user-birthday'>1/4/33</div>
                     <div className='user-address'>25 Menachem, Floor 4, Apt 567</div>
+                </div>
+                <Divider className='divider' />
+                <div className='user-email-wrapper'>
+                    <div className='flex'>
+                        <MailOutlined />
+                        <div className='user-email'>test@gmail.com</div>
+                    </div>
+                    <button className='delete-user'>
+                        <DeleteOutlined />
+                    </button>
                 </div>
             </div>
 
