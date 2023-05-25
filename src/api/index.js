54e64d7f60e4ced964ce20c6172c3b2c11d290a9
rs8030 <rs8030@att.com>
2023-05-25 13:52:47 +0300

    cleanup

--- src/api/index.js
+++ src/api/index.js
@@ -1,32 +1,15 @@
-import axios from 'axios';
-
-const baseUrl = process.env.REACT_APP_API_URL || 'https://jsonplaceholder.typicode.com';
-const loginUrl = process.env.REACT_APP_LOGIN_URL || '';
-
-axios.interceptors.request.use(function (config) {
-    const token = localStorage.getItem('token');
-    config.headers.Authorization =  `Bearer ${token}`;
-    config.baseURL = baseUrl;
-    return config;
-});
-
-export const loginApi = () => {
-    // const {username, password} = data;
-    const data = {
-        email: 'eve.holt@reqres.in',
-        password: 'cityslicka'
-    };
-    return axios.post(`${loginUrl}/api/login`, data);
-};
-
-export const fetchUsersApi = () => {
-    return axios.get('/users');
-};
-
-export const deleteUserApi = data => {
-    return axios.delete('/cyberhat_users', {data: {objectId: data.payload}});
-};
-
-export const createUserApi = data => {
-    return axios.post('/cyberhat_users', {...data.payload});
-};
+// import axios from 'axios';
+//
+// const baseUrl = process.env.REACT_APP_API_URL || 'https://jsonplaceholder.typicode.com';
+//
+// axios.interceptors.request.use(function (config) {
+//     const token = localStorage.getItem('token');
+//     config.headers.Authorization =  `Bearer ${token}`;
+//     config.baseURL = baseUrl;
+//     return config;
+// });
+//
+// export const fetchUsersApi = () => {
+//     return axios.get('/users');
+// };
+//
